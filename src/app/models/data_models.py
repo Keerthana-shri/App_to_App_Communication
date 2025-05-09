152ec1f8cadf05fefc4cd9aac083f58b8f8444d3
Kamalesh <kamalcbe86@gmail.com>
2025-05-09 15:44:52 +0000

    Implemented CRUD operations for Provider API

--- src/app/models/data_models.py
+++ src/app/models/data_models.py
@@ -116,12 +116,6 @@ class Application(Base):
         "ApiKey", back_populates="consumer_app", foreign_keys="ApiKey.consumer_id"
     )
 
-    logs = relationship(
-        "Log",
-        back_populates="application",
-        cascade="all, delete-orphan",
-        passive_deletes=True,
-    )
 
 
 class ApiKey(Base):
@@ -190,9 +184,7 @@ class Log(Base):
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     application_id = Column(
-        UUID(as_uuid=True),
-        ForeignKey("applications.id", ondelete="CASCADE"),
-        nullable=False,
+        UUID(as_uuid=True), ForeignKey("applications.id"), nullable=False
     )
     description = Column(Text, nullable=False)
     created_at = Column(DateTime(timezone=True), default=func.now())
