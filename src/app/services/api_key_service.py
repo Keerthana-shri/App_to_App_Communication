949c0fbbf3aa95ce93cf94efe56d8e339f0da178
Keerthana-Shri <ekeerthanashri@gmail.com>
2025-05-13 00:56:56 +0530

    Modified pagination, corrected validation, modified get_all func

--- src/app/services/api_key_service.py
+++ src/app/services/api_key_service.py
@@ -2,7 +2,11 @@ from datetime import datetime, timezone
 from uuid import UUID, uuid4
 
 from src.app.models.data_models import ApiKey, PermissionEnum, StatusEnum
-from src.app.schemas.api_key_schema import APIKeyDetailResponse, APIKeyResponse
+from src.app.schemas.api_key_schema import (
+    APIKeyDetailResponse,
+    APIKeyListResponse,
+    APIKeyResponse,
+)
 from src.app.services.unit_of_work import APIKeyUnitOfWork
 
 
@@ -21,17 +25,27 @@ class APIKeyService:
         secret_hash: str,
     ) -> dict:
         with self.uow:
+            error_messages = []
+
             consumer_app = self.uow.application.get(consumer_application_id)
             provider_app = self.uow.application.get(provider_application_id)
             api_key_owner = self.uow.user.get(api_key_owner_id)
-            if not consumer_app or not provider_app:
-                raise ValueError("Invalid application ID")
 
-            if consumer_app.secret_hash != secret_hash:
-                raise ValueError("Invalid secret hash")
-
+            if not consumer_app:
+                error_messages.append("Invalid consumer application ID")
+            if not provider_app:
+                error_messages.append("Invalid provider application ID")
+            if consumer_app and consumer_app.secret_hash != secret_hash:
+                error_messages.append("Invalid secret hash")
             if not api_key_owner:
-                raise ValueError("Invalid user to be the owner")
+                error_messages.append("Invalid user to be the owner")
+            if permissions not in PermissionEnum.__members__.values():
+                permissions = PermissionEnum.read
+            if expires_at < datetime.now(timezone.utc):
+                error_messages.append("Invalid expires_at date")
+
+            if error_messages:
+                raise ValueError(", ".join(error_messages))
 
             now = datetime.now(timezone.utc)
             is_active = now <= expires_at
@@ -59,13 +73,25 @@ class APIKeyService:
                 "status": api_key_entry.status,
             }
 
-    def get_all_api_keys(self, page: int = 1, page_size: int = 10):
+    def get_all_api_keys(self, consumer_id: UUID, page: int = 1, page_size: int = 10):
         with self.uow:
-            api_keys, total = self.uow.api_key.get_all(page=page, page_size=page_size)
+            api_keys, total = self.uow.api_key.get_all_by_consumer(
+                consumer_id=consumer_id, page=page, page_size=page_size
+            )
+            total_pages = (total + page_size - 1) // page_size
+            previous_page = page - 1 if page > 1 else None
+            next_page = page + 1 if page < total_pages else None
             api_key_responses = [
                 APIKeyDetailResponse(**api_key.__dict__) for api_key in api_keys
             ]
-            return api_key_responses, total
+            return APIKeyListResponse(
+                total_pages=total_pages,
+                previous_page=previous_page,
+                current_page=page,
+                next_page=next_page,
+                page_size=page_size,
+                items=api_key_responses,
+            )
 
     def get_api_key(self, api_key_id: UUID):
         with self.uow:
