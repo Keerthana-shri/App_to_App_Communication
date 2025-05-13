fb82ecba86ab3794f6eee1fe599a6cb0c3c692c0
Kamalesh <kamalcbe86@gmail.com>
2025-05-13 14:31:48 +0000

    Store encrypted secret as a string instead of bytes
    - Modified encryption logic to use `.decode()` for proper string storage
    - removed previous alembic revision

--- src/app/services/consumer_services.py
+++ src/app/services/consumer_services.py
@@ -45,7 +45,7 @@ def register_consumer(unit_of_work: UnitOfWork, data: ConsumerRegisterRequest):
 
         # Hash the application secret using bcrypt
         secret_code = str(data.application_secret)
-        encrypted_secret = cipher_suite.encrypt(secret_code.encode())
+        encrypted_secret = cipher_suite.encrypt(secret_code.encode()).decode()
 
         uow.application.add(
             id=data.application_guid,
