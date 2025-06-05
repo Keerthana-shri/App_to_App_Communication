fe27ee35d732bf10dc8a26d984354cc50b256d53
Abhishek Manjhi <abhishekmanjhi.1602@gmail.com>
2025-06-05 17:00:47 +0530

    feat: enhance data models with user roles and ownership attributes in User and Application classes

--- src/app/models/data_models.py
+++ src/app/models/data_models.py
@@ -46,17 +46,31 @@ class PermissionEnum(enum.Enum):
     both = "both"
 
 
+class RoleEnum(enum.Enum):
+    """
+    Enum representing the role of a user.
+    - admin: User with administrative privileges.
+    - user: Regular user with standard privileges.
+    """
+
+    admin = "admin"
+    user = "user"
+
+
 class User(Base):
     """
     Represents a user in the system.
+
     Attributes:
         id (UUID): Primary key.
         name (str): Name of the user.
-        email (str): Email address of the user (unique).
+        email (str): Unique email address of the user.
+        password (str): Hashed password of the user.
+        role (RoleEnum): Role of the user (admin or user).
         created_at (datetime): Timestamp when the user was created.
         updated_at (datetime): Timestamp when the user was last updated.
-        applications (list): List of applications owned by the user.
-        api_keys (list): List of API keys owned by the user.
+        applications (list[Application]): Applications owned by the user.
+        api_keys (list[Consumer]): API keys owned by the user.
     """
 
     __tablename__ = "users"
@@ -65,16 +79,17 @@ class User(Base):
     name = Column(String(100), nullable=False)
     email = Column(String(120), unique=True, nullable=False)
     password = Column(String(120), nullable=False)
+    role = Column(Enum(RoleEnum), default=RoleEnum.user)
     created_at = Column(DateTime(timezone=True), default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
 
     applications = relationship("Application", back_populates="owner")
-    api_keys = relationship("ApiKey", back_populates="owner")
 
 
 class Application(Base):
     """
     Represents an application in the system.
+
     Attributes:
         id (UUID): Primary key.
         name (str): Name of the application.
@@ -82,13 +97,15 @@ class Application(Base):
         type (AppType): Type of the application (provider or consumer).
         status (StatusEnum): Status of the application.
         comment (str): Additional comments about the application.
-        user_id (UUID): Foreign key referencing the owner (User).
+        owner_id (UUID): Foreign key referencing the owner (User).
         created_at (datetime): Timestamp when the application was created.
         updated_at (datetime): Timestamp when the application was last updated.
+        created_by (UUID): User who created the application.
+        updated_by (UUID): User who last updated the application.
         owner (User): The user who owns the application.
-        provided_keys (list): API keys provided by the application.
-
-        logs (list): Logs associated with the application.
+        provided_keys (list[Consumer]): API keys provided by this application.
+        consumed_keys (list[Consumer]): API keys consumed by this application.
+        logs (list[Log]): Logs associated with the application.
     """
 
     __tablename__ = "applications"
@@ -100,17 +117,23 @@ class Application(Base):
     status = Column(Enum(StatusEnum), default=StatusEnum.active)
     comment = Column(Text)
 
-    user_id = Column(
+    owner_id = Column(
         UUID(as_uuid=True), ForeignKey("users.id", ondelete="CASCADE"), nullable=False
     )
     created_at = Column(DateTime(timezone=True), default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
 
+    created_by = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
+    updated_by = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
+
     owner = relationship("User", back_populates="applications", passive_deletes=True)
 
     provided_keys = relationship(
-        "ApiKey", back_populates="provider_app", foreign_keys="ApiKey.provider_id"
+        "Consumer", back_populates="provider_app", foreign_keys="Consumer.provider_id"
     )
+
+    consumed_keys = relationship(
+        "Consumer", back_populates="consumer_app", foreign_keys="Consumer.consumer_id")
 
     logs = relationship(
         "Log",
@@ -127,14 +150,18 @@ class Consumer(Base):
     Attributes:
         id (UUID): Primary key.
         provider_id (UUID): Foreign key referencing the provider application.
+        consumer_id (UUID): Foreign key referencing the consumer application.
+        owner_id (UUID): Foreign key referencing the owner (User).
         status (StatusEnum): Status of the API key.
-        api_key_owner_id (UUID): Foreign key referencing the owner (User).
         permissions (PermissionEnum): Permissions associated with the API key.
         created_at (datetime): Timestamp when the API key was created.
         updated_at (datetime): Timestamp when the API key was last updated.
         expires_at (datetime): Expiration timestamp of the API key.
+        created_by (UUID): User who created the API key.
+        updated_by (UUID): User who last updated the API key.
         comment (str): Additional comments about the API key.
         provider_app (Application): The provider application associated with the API key.
+        consumer_app (Application): The consumer application associated with the API key.
         owner (User): The user who owns the API key.
     """
 
@@ -142,28 +169,37 @@ class Consumer(Base):
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     provider_id = Column(
-        UUID(as_uuid=True), ForeignKey("applications.id"), nullable=False
+        UUID(as_uuid=True), ForeignKey("applications.id", ondelete="CASCADE"), nullable=False
+    )
+    consumer_id = Column(
+        UUID(as_uuid=True), ForeignKey("applications.id", ondelete="CASCADE"), nullable=False
+    )
+    owner_id = Column(
+        UUID(as_uuid=True), ForeignKey("users.id", ondelete="CASCADE"), nullable=False
     )
     status = Column(Enum(StatusEnum), default=StatusEnum.active)
-    api_key_owner_id = Column(
-        UUID(as_uuid=True), ForeignKey("users.id"), nullable=False
-    )
     permissions = Column(Enum(PermissionEnum), nullable=False)
 
     created_at = Column(DateTime(timezone=True), default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
     expires_at = Column(DateTime(timezone=True))
+    created_by = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
+    updated_by = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
     comment = Column(Text)
 
     provider_app = relationship(
-        "Application", foreign_keys=[provider_id], back_populates="provided_keys"
+        "Application", foreign_keys=[provider_id], back_populates="provided_keys", passive_deletes=True
     )
-    owner = relationship("User", back_populates="api_keys")
+    consumer_app = relationship(
+        "Application", foreign_keys=[consumer_id], back_populates="consumed_keys", passive_deletes=True
+    )
+    owner = relationship("User", back_populates="api_keys", foreign_keys=[owner_id], passive_deletes=True)
 
 
 class Log(Base):
     """
     Represents a log entry in the system.
+
     Attributes:
         id (UUID): Primary key.
         application_id (UUID): Foreign key referencing the application.
